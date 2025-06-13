b5db0d2f4315c9a499391fa086cd3adc686074cb
Krizhere <imkriz001@gmail.com>
2025-06-13 13:09:23 +0530

    Add files via upload

---
+++ employee-System/src/main/java/com/kiet/AIML_2B_FirstRestAPI/controller/StudentControllerMVC.java
@@ -0,0 +1,65 @@
+package com.kiet.AIML_2B_FirstRestAPI.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.kiet.AIML_2B_FirstRestAPI.model.Student;
+import com.kiet.AIML_2B_FirstRestAPI.service.StudentService;
+
+@Controller
+public class StudentControllerMVC {
+	private StudentService service;
+
+	public StudentControllerMVC(StudentService service) {
+		super();
+		this.service = service;
+	}
+	
+	
+//http://localhost:9090/students/update/102
+	@GetMapping("/students/update/{id}")
+	public String getStudent(Model model,@PathVariable int id) {
+		Student stud = service.retrieveStudentByid(id);
+		model.addAttribute("student", stud);
+		return "student";
+	}
+	
+	@GetMapping(path={"/students","/"})
+	public String getAllStudent(Model model) {
+		
+		List <Student> studs = service.retrieveAllStudent();
+//		Collections.sort(studs);
+		model.addAttribute("students", studs);
+		return "index";
+	}
+//http://localhost:9090/students/add
+@GetMapping("/students/add")
+public String addPage(Model model) {
+	model.addAttribute("student", new Student());
+	return "student";
+}
+	
+	//	
+@PostMapping("/students/save")
+	public String storeStudent(@ModelAttribute Student student) {
+	
+		service.saveStudent(student);
+		return "redirect:/";
+		
+	}
+//http://localhost:9090/students/delete/106	
+@GetMapping("/students/delete/{id}")
+public String removeStudent(@PathVariable int id) {
+	service.remove(id);
+	return "redirect:/";
+	}
+
+}
